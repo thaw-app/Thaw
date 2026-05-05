585085bf41769d451f37c87125d724b474387f09
rene <diaz.de.sandi.7@gmail.com>
2026-05-04 21:02:02 -0700

    refactor: update localized string handling for menu bar section visibility

--- Thaw/MenuBar/MenuBarSection.swift
+++ Thaw/MenuBar/MenuBarSection.swift
@@ -38,7 +38,11 @@ final class MenuBarSection {
 
         /// Localized string key representation.
         var localized: LocalizedStringKey {
-            LocalizedStringKey(displayString)
+            switch self {
+            case .visible: "Visible"
+            case .hidden: "Hidden"
+            case .alwaysHidden: "Always-Hidden"
+            }
         }
     }
 
